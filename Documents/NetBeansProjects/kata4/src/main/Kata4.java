866ee47ae372607f680633eebd6a0ad2cb0248c8
FerPellicer <fernandopellicer782@gmail.com>
2022-12-30 16:09:55 +0000

    Kata 4 Versión 2

--- Documents/NetBeansProjects/kata4/src/main/Kata4.java
+++ Documents/NetBeansProjects/kata4/src/main/Kata4.java
@@ -2,7 +2,6 @@ package main;
 
 import java.io.IOException;
 import java.util.List;
-import view.*;
 import model.*;
 import static view.MailHistogramBuilder.build;
 import static view.MailListReader.read;
@@ -11,13 +10,36 @@ import static view.MailListReader.read;
 public class Kata4 {
 
     public static void main(String[] args) throws IOException {
-        Histogram<String> histogram = new Histogram<>();
-        String fileName = "C:/Users/ferna/Documents/NetBeansProjects/Kata4/email.txt";
-        List<Mail> mailList = read(fileName);
-        histogram = build(mailList);
         
-        HistogramDisplay histo = new HistogramDisplay("", histogram);
-        histo.execute();
+        Kata4 histo = new Kata4();
+        try{
+            histo.execute();
+        }catch (IOException e){
+            System.out.println (e.toString());
+        }
+        
         
     }    
+    
+    private List<Mail> mailList;
+    private Histogram<String> histogram = new Histogram<>();
+    
+    private void execute() throws IOException {
+        input();
+        process();
+        output();
+    }
+    
+    private void input() throws IOException {
+        mailList = read("C:/Users/ferna/Documents/NetBeansProjects/Kata4/email.txt");
+    }
+    
+    private void process() throws IOException {
+        histogram = build(mailList);
+    }
+    
+    private void output() throws IOException {
+        HistogramDisplay histo = new HistogramDisplay("", histogram);
+        histo.execute();
+    }
 }
